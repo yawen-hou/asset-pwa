581f97c17368a7616a9e3a1113dd27b885097f9a
yawen-hou <g2504i@hotmail.com>
2026-03-25 16:53:43 +0800

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'asset-v1';
+const CACHE = 'asset-v2';
 const ASSETS = ['/', '/index.html', '/manifest.json', '/icons/icon-192.png', '/icons/icon-512.png'];
 
 self.addEventListener('install', e => {
